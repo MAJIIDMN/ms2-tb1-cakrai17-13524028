0c9e98e762dfdc0e095bab160ee1f2f7ce81644a
root <root@DESKTOP-IMMDGJC.localdomain>
2025-08-03 20:50:33 +0700

    feats: build

--- pkg_13524028/launch/milestone2.launch.py
+++ pkg_13524028/launch/milestone2.launch.py
@@ -9,7 +9,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # Get the path to the config file
     config_file = os.path.join(
-        get_package_share_directory('magang_2025'),
+        get_package_share_directory('pkg_13524028'),
         'config',
         'config.yaml'
     )
@@ -17,7 +17,7 @@ def generate_launch_description():
     return LaunchDescription([
         # Launch the twist_command_randomizer node
         Node(
-            package='magang_2025',
+            package='pkg_13524028',
             executable='twist_command_randomizer.py',
             name='twist_command_randomizer',
             parameters=[config_file],
@@ -26,7 +26,7 @@ def generate_launch_description():
         
         # Launch the movement_reader node
         Node(
-            package='magang_2025',
+            package='pkg_13524028',
             executable='movement_reader.py',
             name='movement_reader',
             parameters=[config_file],
